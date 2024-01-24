4a8d5f38515c53cecce968dcdcf4fa50a64d8139
Triicky <mrtriickyx@gmail.com>
2024-01-24 21:00:11 +0100

    feat: add functionnal temporary chat

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -1,3 +1,14 @@
 export type ApiChat = {
   streamId: string
 }
+
+export type ChatMessage = {
+  role: string
+  content: string
+  date: string
+}
+
+export type CurrentChat = {
+  title: string
+  messages: ChatMessage[]
+}
