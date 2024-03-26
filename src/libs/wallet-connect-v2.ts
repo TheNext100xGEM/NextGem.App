4d82434c3d08048a5dddd605136d9f7d8a89dac1
leonace924 <meobius123@gmail.com>
2024-03-26 13:59:34 -0400

    fix: include the default chain id

--- src/libs/wallet-connect-v2.ts
+++ src/libs/wallet-connect-v2.ts
@@ -22,7 +22,7 @@ export function buildWalletConnectConnector() {
           // projectId: WALLET_CONNECT_PROJECT_ID,
           projectId: "2b9b340fa18e8b5d57fb3ed8e9821bd3",
           chains: [mainnet],
-          optionalChains,
+          optionalChains: [mainnet, ...optionalChains],
           showQrModal: true
         },
         onError: walletConnectError
