3f1d1a803877f3f1f4fb7d08ec0e98f7d1cff863
fafifox <13659401+fafifox@users.noreply.github.com>
2024-03-29 18:34:25 +0100

    feat: revamp progress bar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,31 +1,37 @@
 import "./_progressbar.scss"
-import Magic  from '@components/Magic'
-import { Corner } from '@components/ui'
-import { PropsProgressBar } from '@models/ProgressBar'
+import Magic from "@components/Magic"
+import { Corner } from "@components/ui"
+import { PropsProgressBar } from "@models/ProgressBar"
 
-const ProgressBar = (
-    { 
-        total,
-        hasMagic
-    }: PropsProgressBar
-
-    ) => {
-        total = total === undefined ? 100 : total
+const ProgressBar = ({ total, hasMagic }: PropsProgressBar) => {
+  total = total === undefined ? 100 : total
   return (
-    <div className="ProgressBar-container">
-        <div className="bar-outer_wrapper">
-            <div className="bar-inner_wrapper" style={{width: 'calc(45px + ' + total + '%)'}}>
-                <div className="bar-percentage">{total + '%'}</div>
-            </div>
+    <div className='ProgressBar-container'>
+      <div className='bar-outer_wrapper'>
+        <div
+          className='bar-inner_wrapper shrinker timelapse'
+          style={
+            {
+              //width: 'calc(45px + ' + total + '%)',
+              "--total": `${100 - total}%`
+            } as React.CSSProperties
+          }
+        ></div>
+        <div className='bar-percentage'>{total + "%"}</div>
+        <div className='bar-gradient' />
+        <Corner />
+      </div>
+      <div
+        className='bar-blur'
+        style={{ left: "calc(" + total + "% - 75px + 22px)" }}
+      />
 
-            <Corner />
-
-        </div>
-        <div className="bar-blur" style={{left: 'calc(' + total + '% - 75px + 22px)'}}/>
-
-        <div className="magic-container" style={{left: 'calc(' + total + '% - 75px + 22px)'}}>
+      <div
+        className='magic-container'
+        style={{ left: "calc(" + total + "% - 75px + 22px)" }}
+      >
         {hasMagic && <Magic />}
-        </div>
+      </div>
     </div>
   )
 }
